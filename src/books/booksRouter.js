a95260b9d4d85bb2d933238569367b5cd7ee9f6a
nikki-ai <shony1198@gmail.com>
2021-01-05 21:35:02 -0600

    first commit, auth key broken

---
+++ src/books/booksRouter.js
@@ -0,0 +1,82 @@
+'use strict';
+
+const express = require('express');
+const { v4: uuid } = require('uuid');
+const logger = require('../logger');
+const { bookmarks } = require('../store');
+
+const booksRouter = express.Router();
+
+booksRouter
+  .route('/bookmarks')
+  .get((req, res) => {
+    res.json(bookmarks);
+  })
+  .post((req, res) => {
+    const { title, url, description, rating} = req.body;
+
+    if(!title) {
+      logger.error('Title is required');
+      return res.status(400).send('Invalid data');
+    }
+
+    if(!url) {
+      logger.error('Url is required');
+      return res.status(400).send('Invalid data');
+    }
+
+    if(!description) {
+      logger.error('Description is required');
+      return res.status(400).send('Invalid data');
+    }
+
+    if(!rating) {
+      logger.error('Rating is required');
+      return res.status(400).send('Invalid data');
+    }
+
+    const id = uuid();
+    const bookmark = {
+      id, 
+      title, 
+      url, 
+      description, 
+      rating
+    };
+
+    bookmarks.push(bookmark);
+    logger.info(`Card with id ${id} created`);
+
+    res.status(201).location(`http://localhost:8000/bookmarks/${id}`).json(bookmark);
+  });
+  
+
+booksRouter
+  .route('/bookmarks/:id')
+  .get((req, res) => {
+    const { id } = req.params;
+    const bookmark = bookmarks.find(b => b.id == id);
+
+    if(!bookmark) {
+      logger.error(`Bookmark with id ${id} not found.`);
+      return res.status(404).send('Bookmark not found');
+    }
+
+    res.json(bookmark);
+  })
+  .delete((req, res) => {
+    const { id } = req.params;
+    const bookmarkIndex = bookmarks.findIndex(b => b.id == id);
+
+    if (bookmarkIndex === -1) {
+      logger.error(`Bookmark with id ${id} not found.`);
+      return res.status(404).send('Not found');
+    }
+
+    bookmarks.splice(bookmarkIndex, 1);
+
+    logger.info(`Bookmark with id ${id} deleted.`);
+    res.status(204).end();
+  });
+
+module.exports = booksRouter;
